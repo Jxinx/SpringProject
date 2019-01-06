97061311ba5bfed3300b72b94a83a5291822881b
Jxinx <Jxinx@foxmail.com>
2019-01-06 18:09:36 +0800

    Eureka server-user project

---
+++ service-user/src/main/java/com/github/jxinx/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.github.jxinx.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+    private final Logger logger = Logger.getLogger(getClass());
+
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "/hello")
+    public String hello() {
+        ServiceInstance instance = client.getLocalServiceInstance();
+        logger.info("/hello, host:" + instance.getHost() + ", service_id:" + instance.getServiceId());
+        return "Hello World";
+    }
+}
