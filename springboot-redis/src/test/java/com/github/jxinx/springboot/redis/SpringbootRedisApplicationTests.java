eb6b57dba2cc80e9f4eb037a6f9f7d60aa529a94
Jxinx <Jxinx@foxmail.com>
2020-09-30 11:53:53 +0800

    springboot整合redis项目

---
+++ springboot-redis/src/test/java/com/github/jxinx/springboot/redis/SpringbootRedisApplicationTests.java
@@ -0,0 +1,14 @@
+package com.github.jxinx.springboot.redis;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
