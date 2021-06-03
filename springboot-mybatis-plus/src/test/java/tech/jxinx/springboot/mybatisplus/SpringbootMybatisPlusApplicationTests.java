2d4fabead58b4a62da72a131a3bf6b5dc64b294d
J_xinx <1239684987@qq.com>
2021-06-03 16:49:56 +0800

    MP分页插件quickstart

--- springboot-mybatis-plus/src/test/java/tech/jxinx/springboot/mybatisplus/SpringbootMybatisPlusApplicationTests.java
+++ springboot-mybatis-plus/src/test/java/tech/jxinx/springboot/mybatisplus/SpringbootMybatisPlusApplicationTests.java
@@ -1,5 +1,6 @@
 package tech.jxinx.springboot.mybatisplus;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,6 +20,25 @@ class SpringbootMybatisPlusApplicationTests {
         // 查询所有
         List<RoleInfo> roleInfos = roleinfoMapper.selectList(null);
         roleInfos.forEach(System.out::println);
+    }
+
+    @Test
+    void testSql() {
+        // 查询所有
+        Page<RoleInfo> roleInfoPage = new Page<>();
+        roleInfoPage.setCurrent(1);
+        roleInfoPage.setSize(3);
+
+        roleinfoMapper.selectPage(roleInfoPage, null);
+
+        // 总记录数
+        System.out.println("roleInfoPage.getTotal() = " + roleInfoPage.getTotal());
+        // 总页数
+        System.out.println("roleInfoPage.getPages() = " + roleInfoPage.getPages());
+
+        // 数据列表
+        List<RoleInfo> records = roleInfoPage.getRecords();
+        records.forEach(System.out::println);
     }
 
 }
